a91640df5e9feb93c49cf8f7641fec2670a5bcca
Vaibhav Sahu <vaibhavsahu200@gmail.com>
2024-05-14 20:31:05 +0530

    added scroll functionality

--- src/Components/Shop/Shop.jsx
+++ src/Components/Shop/Shop.jsx
@@ -1,54 +1,65 @@
-import React, { useState } from 'react'
-import Categories from "./Categories"
+import React, { useState, useEffect } from 'react';
+import Categories from "./Categories";
 import CategoryCard from './CategoryCard';
 
 function Shop() {
     const data = Categories;
     const [scrollPosition, setScrollPosition] = useState(0);
 
-    const scrollLeft = () => {
+    useEffect(() => {
         const container = document.querySelector('.scroll-container');
         if (container) {
-        setScrollPosition(scrollPosition - 50); // Scroll left by 50 pixels
-        container.scrollLeft -= 50;
+            container.scrollLeft = scrollPosition;
+        }
+    }, [scrollPosition]);
+
+    const handleScroll = () => {
+        const container = document.querySelector('.scroll-container');
+        if (container) {
+            setScrollPosition(container.scrollLeft);
+        }
+    };
+
+    const scrollLeft = () => {
+        const container = document.querySelector('.scroll-container');
+        if (container && scrollPosition > 0) {
+            setScrollPosition(scrollPosition - 100);
         }
     };
 
     const scrollRight = () => {
         const container = document.querySelector('.scroll-container');
         if (container) {
-        setScrollPosition(scrollPosition + 50); // Scroll right by 50 pixels
-        container.scrollLeft += 50;
+            setScrollPosition(scrollPosition + 100);
         }
     };
 
     const categoriesData = data.map((item, index) => {
         return <CategoryCard
-            key = {index} 
-            item = {item}
+            key={index}
+            item={item}
         />
-    })
+    });
 
-  return (
-    <div className='relative scroll-container'>
-        <main className='flex flex-col mx-[50px] '>
-            <div className='flex justify-between'>
-                <h2 className='font-semibold text-[30px]'>Shop Groceries</h2>
-                <a className='underline' href="">View all</a>
-            </div>
-            <div className='flex overflow-auto'>
-                {categoriesData}
-            </div>
-        </main>
-        <button onClick={scrollLeft} className='absolute left-0 top-0 h-full  text-gray-600  hover:text-gray-700 px-3 flex items-center justify-center'>
-            <img className='' src='/left-arrow.svg' alt="" />
-        </button>
-        <button onClick={scrollRight} className='absolute right-0 top-0 h-full  text-gray-600  hover:text-gray-700 px-3 flex items-center justify-center'>
-            <img className='' src='/right-arrow.svg' alt="" />
-        </button>
-    </div>
-    
-  )
+    return (
+        <div className='relative '>
+            <main className='flex flex-col mx-[50px]'>
+                <div className='flex justify-between'>
+                    <h2 className='font-semibold text-[30px]'>Shop Groceries</h2>
+                    <a className='underline' href="">View all</a>
+                </div>
+                <div onScroll={handleScroll} className='flex overflow-auto scroll-container scrollbar-thin scroll-smooth'>
+                    {categoriesData}
+                </div>
+            </main>
+            <button onClick={scrollLeft} className='absolute left-0 top-0 h-full  text-gray-600  hover:text-gray-700 px-3 flex items-center justify-center'>
+                <img className='' src='/left-arrow.svg' alt="" />
+            </button>
+            <button onClick={scrollRight} className='absolute right-0 top-0 h-full  text-gray-600  hover:text-gray-700 px-3 flex items-center justify-center'>
+                <img className='' src='/right-arrow.svg' alt="" />
+            </button>
+        </div>
+    );
 }
 
-export default Shop
+export default Shop;
