fb1a4a4cf9f2287caadff444bcf20f7410d928c6
Vaibhav Sahu <vaibhavsahu200@gmail.com>
2024-05-15 15:53:24 +0530

    changed carousel components

--- src/Components/Hero/Component2.jsx
+++ src/Components/Hero/Component2.jsx
@@ -3,12 +3,12 @@ import React from 'react'
 function Component2() {
   return (
     <section className='bg-cover bg-no-repeat h-[500px] min-w-[100%] flex flex-col justify-center items-center p-[30px] gap-[20px]' style={{backgroundImage: "url('/Hero/image2.jpg')"}}>
-      <h4 className='font-semibold text-[40px] text-[black]'>Taste & Authentic</h4>
-      <h2 className='font-bold text-[80px] text-black'>A Salad in Action</h2>
+      <h4 className='font-semibold text-[40px] text-[white]'>Taste & Authentic</h4>
+      <h2 className='font-bold text-[60px] text-white'>Tastes & Spices</h2>
       <p className='text-[40px] font-semibold text-[white]'>Made the Traditional Way</p>
       <div className='flex justify-start items-center gap-[10px]'>
         <button className='bg-[#1F7C3E] px-[30px] py-[15px] text-white rounded-md font-semibold'>Shop Fruits & Vegetables</button>
-        <button className='bg-[#1F7C3E] px-[30px] py-[15px] text-white rounded-md font-semibold'>Store Cupboard</button>
+        <button className='bg-[#1F7C3E] px-[30px] py-[15px] text-white rounded-md font-semibold'>Fresh Ingridents</button>
       </div>
     </section>
   )
